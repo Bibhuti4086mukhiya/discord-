1bf3c10a040560165a494e871d860a3e6a107e1d
Bibhuti Bhushan Mukhiya <bibhutibhushanmukhiya4086@gmail.com>
2023-08-20 21:44:52 +0545

    first commit

---
+++ d.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+# Define the intents you need
+intents = discord.Intents.default()
+intents.typing = True  # You can adjust this based on your needs
+
+# Create a bot instance with intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Event: Bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    
+# Command: Extract Messages
+@bot.command()
+async def extract_messages(ctx, channel: discord.TextChannel):
+    messages = await channel.history(limit=None).flatten()
+    with open('messages.txt', 'w', encoding='utf-8') as file:
+        for message in messages:
+            file.write(f"{message.author.name}: {message.content}\n")
+
+# Run the bot
+bot.run('MTE0MDI0MDEwMDAwNjA1MTk1MQ.G9RXOZ.thG2SYkeidmCSml_jMGDDxm3JsavV1ahQyW-_0')
