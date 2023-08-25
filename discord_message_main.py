89a79a3fe8ee8a875ffd51916b090c52792121b6
Bibhuti Bhushan Mukhiya <bibhutibhushanmukhiya4086@gmail.com>
2023-08-25 17:35:29 +0545

    done

--- discord_message_main.py
+++ discord_message_main.py
@@ -13,7 +13,7 @@ import json
 
 def discord_message(channel_id):
     headers={
-        'authorization':'MTA0MTI3MTA1MDIwMzMwODA1NQ.GqjmFD.zuEV8eJodVQFRnCPLj69O6x1STdjHjjC_M9gnQ'
+        'authorization':''
     }
     response=requests.get(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=headers)
     jsson=json.loads(response.text)
